093bb4c9ede0860e5798f3997cc9bd7575cabed6
iMaxwe11 <mnixon112@outlook.com>
2026-04-12 11:15:28 -0400

    cleanup: remove temp scripts

--- deploy.bat
+++
@@ -1,6 +0,0 @@
-@echo off
-cd /d C:\Projects\maxwell-tech-hub
-git add -A
-git commit -m "Arcade expansion: hub page, Neon Pong, Memory Matrix, Reaction Time, Type Racer"
-git push origin main
-del "%~f0"
