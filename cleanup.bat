bc59d3f600cb5958d0c2c0ca0de2340f4e2364ae
iMaxwe11 <mnixon112@outlook.com>
2026-04-12 11:02:04 -0400

    cleanup: remove temp deploy script

--- cleanup.bat
+++ cleanup.bat
@@ -1,6 +1,6 @@
 @echo off
 cd /d C:\Projects\maxwell-tech-hub
 git add -A
-git commit -m "Site upgrade: visual polish, SEO, Space page, 404, loading skeleton"
+git commit -m "cleanup: remove temp deploy script"
 git push origin main
 del "%~f0"
